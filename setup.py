3af48a11beb8df54b082b30233a2ee173c284bb7
Code3rr0r <maxbohanan880@gmail.com>
2025-01-15 16:09:37 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="pykiller",
     version="0.2",  # Increment the version
     packages=["pykiller"],
+    long_description="PYKILLER is a stress-testing tool that simulates extreme system conditions to test stability and performance under heavy loads. Use responsibly in isolated, non-production environments. .",
+    long_description_content_type="text/x-rst",  # or 'text/markdown' based on your file type
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
